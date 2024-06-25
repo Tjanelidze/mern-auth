8d3baf868836d60d42a7aa5cedaa201e1c82e52a
Tedo <tedojanelidze7@gmail.com>
2024-06-25 22:42:32 +0400

    implemented login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,21 @@ import User from '../models/userModel.js';
 // route    POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Auth user' });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(200).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
 });
 
 // @desc Register a new user
