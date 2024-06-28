d599b208201e9705cd826c4358c6eaeff7429c94
Tedo <tedojanelidze7@gmail.com>
2024-06-28 18:24:01 +0400

    implemented hero

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
   return (
     <>
       <Header />
-      <h1>MERN Auth</h1>
+      <HomeScreen />
     </>
   );
 }
